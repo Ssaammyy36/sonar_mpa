6688f4fbb8549b6b623cb741532147504889121b
Samson Wagner <Samson.Wagner@online.de>
2025-07-23 21:49:58 +0200

    Klassen erstellt

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
-from utils.logger import get_logger
+from sensor import Sensor
+from empfaenger import Empfaenger
+from datenverarbeitung import Datenverarbeitung
+from steuerung import Steuerung
+import logging
 
-# Logger = besser asl print für übersichlichliche Ausgaben
-logger = get_logger(__name__)
+def teste_klassen( sensor, empfaenger, daten, steuerung):
+    sensor.test()
+    empfaenger.test()
+    daten.test()
+    steuerung.test()
 
 def main():
-    logger.info("Programm startet.")
-    # ...
-    logger.warning("Dies ist nur ein Beispiel.")
-    logger.error("Hier ist ein Fehler aufgetreten!")
+    # Teste alle Klassen
+    sensor = Sensor("S1")
+    empfaenger = Empfaenger("E1")
+    daten = Datenverarbeitung()
+    steuerung = Steuerung()
+
+    teste_klassen(sensor, empfaenger, daten, steuerung) 
 
 if __name__ == "__main__":
     main()

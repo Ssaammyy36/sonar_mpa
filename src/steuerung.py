b538adabc915522016ba4189258e1593389eb3a7
Samson Wagner <Samson.Wagner@online.de>
2025-08-06 23:28:38 +0200

    Zentralisiere Anwendungslogik in Steuerungsklasse

--- src/steuerung.py
+++ src/steuerung.py
@@ -1,18 +1,33 @@
 from utils.logger import get_logger
+from sensor import Sensor
+from empfaenger import Empfaenger
+from datenverarbeitung import Datenverarbeitung
 
 class Steuerung:
     """
-    Steuert den Ablauf der Sonar-Messung.
-
-    Attributes:
-        logger: Das Logging-Objekt für diese Klasse.
+    Steuert den gesamten Ablauf der Sonar-Messung und -Verarbeitung.
+    Diese Klasse initialisiert alle notwendigen Komponenten.
     """
     def __init__(self):
-        """
-        Initialisiert ein neues Steuerungs-Objekt.
-        """
+        """Initialisiert die Steuerung und alle Kernkomponenten."""
         self.logger = get_logger(__name__)
+        
+        # Die Steuerung erstellt und besitzt die anderen Objekte
+        self.sensor = Sensor("S1")
+        self.empfaenger = Empfaenger("E1")
+        self.datenverarbeitung = Datenverarbeitung()
+        
+        self.logger.debug("Steuerung und alle Komponenten initialisiert.")
 
-    def test(self):
-        """Loggt eine Test-Nachricht, um die Erreichbarkeit zu prüfen."""
+    def teste_komponenten(self):
+        """Führt einen einfachen Erreichbarkeitstest für alle Komponenten aus."""
+        self.logger.info("Starte Komponenten-Test...")
+        
+        # Hier kommt deine eigentliche Programmlogik hin
+        # Fürs Erste rufen wir die Test-Methoden auf:
+        self.sensor.test()
+        self.empfaenger.test()
+        self.datenverarbeitung.test()
         self.logger.debug("Steuerung erreichbar.")
+        
+        self.logger.info("Komponenten-Test beendet.")

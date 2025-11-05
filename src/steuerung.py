fa0ae477b1be291a9f2b796458b2a30c0e7234b2
Samson Wagner <Samson.Wagner@online.de>
2025-11-05 16:18:26 +0100

    deleted main

--- src/steuerung.py
+++ src/steuerung.py
@@ -86,3 +86,22 @@ class Steuerung:
             self.logger.info(f"{len(amplituden)} Amplituden-Samples erfolgreich geparst.")
         else:
             self.logger.warning("Keine Binärdaten vom Sonar empfangen.")
+
+if __name__ == "__main__":
+    
+    """
+    Hauptfunktion: Erstellt das Steuerungsobjekt und startet die Anwendung.
+    """
+    try:
+        # --- HIER DEN GEWÜNSCHTEN TESTMODUS EINGEBEN ---
+        # '3' oder 'nmea':   für den NMEA-Tiefendaten-Test
+        # '100': für 12-Bit  
+        # '101': für den 8-Bit Binärdaten-Test mit Plot
+
+        test_modus = "3"
+
+        steuerung = Steuerung()
+        steuerung.starte_anwendung(test_modus)
+    except Exception as e:
+        print(f"Ein unerwarteter Fehler ist aufgetreten: {e}", file=sys.stderr)
+        sys.exit(1)

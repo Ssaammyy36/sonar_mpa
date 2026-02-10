40e76c48e9f60befe3c81dcc982588581e0a29c9
Samson Wagner <Samson.Wagner@online.de>
2026-02-10 17:02:24 +0100

    config addes smv

--- src/config.py
+++ src/config.py
@@ -31,14 +31,12 @@ LOGGING_CONFIG = {
 }
 
 # Einstellungen für die KI-Klassifizierung
-# Einstellungen für die KI-Klassifizierung
 ANALYSIS_CONFIG = {
     "enable_classification": True,
     "active_model_id": "random_forest",
     "models": {
         "random_forest": {
             "type": "random_forest",
-<<<<<<< HEAD
             "model_path": "models/advanced_ki.pkl",
             "settings": {
                 "win_len": 155,
@@ -46,25 +44,16 @@ ANALYSIS_CONFIG = {
                 "v_start_hf": 20,
                 "p_mask_lf": 78,
                 "v_start_lf": 55
-=======
-            "model_path": "models/sonar_model_155_noScaler_noPCA.pkl",
-            "settings": {
-                "win_len": 155
->>>>>>> dev
             }
         },
         "lstm": {
             "type": "lstm",
-<<<<<<< HEAD
-            "model_path": "models/lstm_demo.pth",
-=======
-            "model_path": "???",
+            "model_path": "models/lstm.pth",
             "settings": {}
         },
         "svm": {
             "type": "svm",
-            "model_path": "???",
->>>>>>> dev
+            "model_path": "models/svm.pth",
             "settings": {}
         }
     }
